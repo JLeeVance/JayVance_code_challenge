7b005339b4d3c8e92d008cf426aafe6a2da30fcd
Jay Vance <justinleevance94@gamil.com>
2024-07-17 22:17:50 -0400

    created type dictionary

--- parserator_web/views.py
+++ parserator_web/views.py
@@ -11,6 +11,13 @@ from rest_framework.exceptions import ParseError
 
 logger = logging.getLogger(__name__)
 
+types = {
+    'LandmarkName':'major landmark building',
+    'OccupancyType':'multi-unit dwelling',
+    'SubaddressType':'multi-unit dwelling',
+    'USPSBoxID':'PO Box',
+    'SubaddressType':'Non-Residential unit',
+    }
 
 class Home(TemplateView):
     template_name = 'parserator_web/index.html'
@@ -18,19 +25,23 @@ class Home(TemplateView):
 
 class AddressParse(APIView):
     renderer_classes = [JSONRenderer]
-    logger.critical('Critial: HIT')
-    logger.warning('HIT')
     
-
     def get(self, request):
-        address = request.query_params.get('address')
-        logger.warning(address)
+        addr = request.query_params.get('address')
+        parse_values = self.parse(addr)
+        logger.warning('Inside get, after self.parse', parse_values)
         
         # TODO: Flesh out this method to parse an address string using the
         # parse() method and return the parsed components to the frontend.
-        return Response({'message': 'Hello, World'})
+        return Response({'components': parse_values})
 
     def parse(self, address):
+
+        address_components = usaddress.parse(address)
+        logger.warning('Inside self.parse')
+        logger.critical(address_components[0])
+        address_type = ''
+
         # TODO: Implement this method to return the parsed components of a
         # given address using usaddress: https://github.com/datamade/usaddress
         return address_components, address_type
