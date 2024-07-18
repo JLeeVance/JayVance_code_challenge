01c338d110d9917f772728bffc57bece1f8cb7b7
Jay Vance <justinleevance94@gamil.com>
2024-07-18 14:18:25 -0400

    finished logic for frontend rendering

--- parserator_web/static/js/index.js
+++ parserator_web/static/js/index.js
@@ -1,37 +1,83 @@
-/* TODO: Flesh this out to connect the form to the API and render results
-   in the #address-results div. */
+const results_div = document.getElementById('address-results');
+const tBody = document.getElementById('results_table')
+const form = document.getElementById('form');
+const input = document.getElementById('address');
+
+
 document.addEventListener('DOMContentLoaded', (e) => {
-   const form = document.getElementById('form');
-   const input = document.getElementById('address');
-
-
+   
    form.addEventListener('submit', (e) => {
       e.preventDefault();
+      results_div.style.display = 'none';
+      tBody.innerHTML = '';
       const address = input.value
-
+      
       /* complete fetch to backend */
       fetch(`/api/parse/?address=${address}`)
-         .then(r => r.json())
-         .then(parse_response => {
-            addToDOM(parse_response)
-         })
-         .catch(error => {
-            console.log('Error', error)
-         })
-      
-      function addToDOM(parse_response){
-         console.log(parse_response)
-         div = document.getElementById('address-results')
+      .then(r => {
+         if(!r.ok){
+            return r.json().then(errorResponse => {
+               throw new Error(errorResponse.ErrorMessage)
+            })
+         }
+         return r.json();
+      })
+      .then(parse_response => {
+         // console.log('After Good Fetch on Frontend',parse_response)
          
-         
-         
-
-         return
-      }
-      
-
+         addToDOM(parse_response)
+      })
+      .catch(error => {
+         // console.log(error)
+         showErrorMessages(error)
+      })
    });
 })
 
+function createTableRows(addressComponents){
+   console.log(addressComponents)
+   for ([key, value] of Object.entries(addressComponents)){
+      // console.log(key, value)
+      const newRow = document.createElement('tr');
+      const addressPart = document.createElement('td');
+      const tag = document.createElement('td');
+      
+      addressPart.innerText = value;
+      tag.innerText = key;
+      newRow.appendChild(addressPart)
+      newRow.appendChild(tag)
+      tBody.appendChild(newRow)
+   }
+   return
+}
 
-// Add error handling for front end, need to alert user of any error
+function addToDOM(parse_response){
+   const inputString = parse_response.input_string;
+   const addressType = parse_response.address_type;
+   const addressComponents = parse_response.address_components;
+
+   const spanForType = document.getElementById('parse-type')
+   
+   if(spanForType){
+      spanForType.innerHTML = addressType;
+   }
+
+   createTableRows(addressComponents)
+
+   results_div.style = 'display:block'
+   return
+};
+
+function showErrorMessages(error){
+   const message = error.message
+   const errorDiv = document.getElementById('error-div');
+   const errorSpan = document.getElementById('error-message');
+
+   errorSpan.textContent = message;
+   errorDiv.style.display = 'block';
+
+   setTimeout(() => {
+      errorDiv.style.display = 'none';
+      errorSpan.textContent = ''
+   }, 3000);
+}
--- parserator_web/views.py
+++ parserator_web/views.py
@@ -4,21 +4,26 @@ import os
 
 import usaddress
 from django.views.generic import TemplateView
+from django.http import JsonResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
-from rest_framework.exceptions import ParseError
+
+
 
 logger = logging.getLogger(__name__)
 
-# created types dictionary to deduce addr_type 
-types = {
-    'LandmarkName':'major landmark building',
-    'OccupancyType':'multi-unit dwelling',
-    'SubaddressType':'multi-unit dwelling',
-    'USPSBoxID':'PO Box',
-    'SubaddressType':'Non-Residential unit',
-    }
+# created types dictionary to deduce addr_type
+# realized that .tag() would be the correct use
+# of the usaddress methods available
+
+# types = {
+#     'LandmarkName':'major landmark building',
+#     'OccupancyType':'multi-unit dwelling',
+#     'SubaddressType':'multi-unit dwelling',
+#     'USPSBoxID':'PO Box',
+#     'SubaddressType':'Non-Residential unit',
+#     }
 
 class Home(TemplateView):
     template_name = 'parserator_web/index.html'
@@ -29,28 +34,50 @@ class AddressParse(APIView):
     
     def get(self, request):
         addr = request.query_params.get('address')
+
+        if not addr:
+            return Response({
+                'ErrorMessage': 'The input field can not be left empty'
+            }, 400)
+        
         try:
             parse_values, addr_type = self.parse(addr)
-
+            logger.warning('After Parse:')
+            logger.warning(parse_values, addr_type)
+            
             return Response({
                 'input_string': addr,
                 'address_components': parse_values,
                 'address_type': addr_type,
-            }, 200)
-        except:
-            return Response({'Error':'There was an error in your submission'}, 412)     
+                }, 200)
+        
+        # Handles repeated tags within parse() response 
+        except usaddress.RepeatedLabelError as rle:
+            logger.error('Parsing Error:', rle)
+            return Response({
+                'ErrorMessage': 'You must enter a valid US address, be sure there are not repeating values.',
+                }, 412)
+        
+        # Handles all other errors
+        except Exception as e:
+            return Response({
+                'ErrorMessage': 'There was an unexpected error while processing your request, try again.',
+                'ErrorData': e
+                }, 500)
 
     def parse(self, address):
 
-        address_components = usaddress.parse(address)
+        address_components, address_type = usaddress.tag(address)
+        # address_type = 'Residential' #Default
         
-        for pair in address_components:
-            # Checking for a type match to determine addr_type #
-            if pair[1] in types.keys():
-                type = pair[1]
-                # Assign type based on types match value
-                address_type = types[type]
-                break
-            address_type = 'Residential'
+        # for pair in address_components:
+        #     # Checking for a type match to determine addr_type #
+        #     if pair[1] in types.keys():
+        #         type = pair[1]
+        #         # Assign type based on types match value
+        #         address_type = types[type]
+        #         break
+        if 'error' in address:
+            raise ValueError('Invalid address format')
                 
         return address_components, address_type
